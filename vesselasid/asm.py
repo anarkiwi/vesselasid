5d2969335e83c748c43bbf8d11494e2147ab26d5
Josh Bailey <josh@vandervecken.com>
2023-12-10 06:44:07 +0000

    with.

--- vesselasid/asm.py
+++ vesselasid/asm.py
@@ -9,8 +9,8 @@ def xa(code, origin=0xC000):
         code_file = os.path.join(tmpdir, "code.s")
         with open(code_file, "w") as f:
             f.write(full_code)
-        p = Popen(["xa", "-o", "-", code_file], stdout=PIPE, stderr=PIPE)
-        ocode = list(p.communicate()[0])
+        with Popen(["xa", "-o", "-", code_file], stdout=PIPE, stderr=PIPE) as p:
+            ocode = list(p.communicate()[0])
         if not ocode:
             raise ValueError(code)
         return ocode
